3e8ef2452a47de1ef48ae4617c7c062d170229eb
Maria Floruss <maria@floruss.net>
2014-08-23 15:33:04 +0100

    added gameobject.cs, player.cs, square.cs and circle.cs. added dummy texture for circle.

---
+++ source/Squircle/Squircle/Circle.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Squircle
+{
+    class Circle : Player
+    {
+        private Texture2D circleTexture;
+
+
+        public override void LoadContent(ContentManager content)
+        {
+            circleTexture = content.Load<Texture2D>("circle");
+        }
+    }
+}
---
+++ source/Squircle/Squircle/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Squircle
+{
+    class GameObject
+    {
+        protected Game Game { get; private set; }
+
+        public abstract Texture2D Texture { get; }
+        public abstract Vector2 Pos { get; set; }
+
+        public GameObject(Game game)
+        {
+            Game = game;
+        }
+
+        public abstract void Initialize();
+        public abstract void LoadContent(ContentManager content);
+
+        public virtual void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
