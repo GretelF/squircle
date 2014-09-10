11f7eee1462742d6c07f40a363b5a5e9694aad30
Maria Floruss <maria@floruss.net>
2014-09-10 15:05:37 +0100

    deleted level0_triggers.cfg -> merged into level0_objects. moved platform object to its own file. platform texture and collision body now match.

--- source/Squircle/Squircle/GameObject.cs
+++ source/Squircle/Squircle/GameObject.cs
@@ -23,9 +23,11 @@ namespace Squircle
                     resultObject = new PlatformObject(game);
                     break;
                 case "trigger":
-                    throw new NotImplementedException();
+                    resultObject = new TriggerObject(game);
+                    break;
                 case "button":
-                    throw new NotImplementedException();
+                    resultObject = new ButtonObject(game);
+                    break;
                 default:
                 {
                     var message = new StringBuilder();
@@ -101,64 +103,4 @@ namespace Squircle
         }
     }
 
-    public class PlatformObject : GameObject
-    {
-        private Vector2 _pos;
-        private Texture2D _texture;
-        private string _textureName;
-
-        public override Vector2 Pos
-        {
-            get { return _pos; }
-            set { _pos = value;}
-        }
-        public override Texture2D Texture
-        {
-            get { return _texture; }
-        }
-
-        public PlatformObject(Game game) : base(game)
-        {
-        }
-
-        public override void Initialize()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void LoadContent(ContentManager content)
-        {
-            _texture = content.Load<Texture2D>(_textureName);
-        }
-
-        public override void InitializeFromConfig(ConfigSection section)
-        {
-            _textureName = section["texture"];
-            var bounds = section["bounds"].AsRectangle();
-
-            Pos = new Vector2(bounds.X, bounds.Y);
-
-            // TODO physics
-            //Game.level.World
-
-            var bodyDef = new BodyDef();
-            var fixtureDef = new FixtureDef();
-            var shape = new PolygonShape();
-            shape.SetAsBox(bounds.Width/2, bounds.Height/2);
-            fixtureDef.shape = shape;
-            fixtureDef.userData = new LevelElementInfo() { type = LevelElementType.Ground};
-            bodyDef.type = BodyType.Static;
-            bodyDef.position = Pos;
-            Game.level.World.CreateBody(bodyDef).CreateFixture(fixtureDef);
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-        }
-
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(_texture, Pos, Microsoft.Xna.Framework.Color.White);
-        }
-    }
 }
---
+++ source/Squircle/Squircle/TriggerObjects.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Box2D.XNA;
+using Configuration;
+
+namespace Squircle
+{
+
+    public class TriggerObject : GameObject
+    {
+        private Vector2 _pos;
+        private Texture2D _texture;
+        private string _textureName;
+
+        public override Vector2 Pos
+        {
+            get { return _pos; }
+            set { _pos = value; }
+        }
+        public override Texture2D Texture
+        {
+            get { return _texture; }
+        }
+
+        public TriggerObject(Game game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            _texture = content.Load<Texture2D>(_textureName);
+        }
+
+        public override void InitializeFromConfig(ConfigSection section)
+        {
+            _textureName = section["texture"];
+            Pos = section["position"].AsVector2();
+            var dim = section["dimensions"].AsVector2();
+
+            var bodyDef = new BodyDef();
+            var fixtureDef = new FixtureDef();
+            var shape = new PolygonShape();
+            shape.SetAsBox(dim.X / 2, dim.Y / 2);
+            fixtureDef.shape = shape;
+            fixtureDef.isSensor = true;
+            fixtureDef.userData = new LevelElementInfo() { type = LevelElementType.Ground };
+            bodyDef.position = Pos;
+            Game.level.World.CreateBody(bodyDef).CreateFixture(fixtureDef);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Pos, Microsoft.Xna.Framework.Color.White);
+        }
+    }
+    
+    public class ButtonObject : GameObject
+    {
+        private Vector2 _pos;
+        private Texture2D _texture;
+        private string _textureName;
+
+        public override Vector2 Pos
+        {
+            get { return _pos; }
+            set { _pos = value;}
+        }
+        public override Texture2D Texture
+        {
+            get { return _texture; }
+        }
+
+        public ButtonObject(Game game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            _texture = content.Load<Texture2D>(_textureName);
+        }
+
+        public override void InitializeFromConfig(ConfigSection section)
+        {
+            _textureName = section["texture"];
+            Pos = section["position"].AsVector2();
+            var dim = section["dimensions"].AsVector2();
+
+            var bodyDef = new BodyDef();
+            var fixtureDef = new FixtureDef();
+            var shape = new PolygonShape();
+            shape.SetAsBox(dim.X / 2, dim.Y / 2);
+            fixtureDef.shape = shape;
+            fixtureDef.isSensor = true;
+            fixtureDef.userData = new LevelElementInfo() { type = LevelElementType.Ground};
+            bodyDef.position = Pos;
+            Game.level.World.CreateBody(bodyDef).CreateFixture(fixtureDef);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Pos, Microsoft.Xna.Framework.Color.White);
+        }
+    }
+}
